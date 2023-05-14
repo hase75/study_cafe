2d557da09ec6b598cd50b313d0b92cb8ee7bde3d
hase75 <122374696+hase75@users.noreply.github.com>
2023-05-14 08:42:49 +0000

    [Update]ransackを使った絞り込み検索の修正

--- app/controllers/public/spaces_controller.rb
+++ app/controllers/public/spaces_controller.rb
@@ -3,9 +3,9 @@ class Public::SpacesController < ApplicationController
   def index
     @q = Space.ransack(params[:q])
     if params[:keyword]
-      @spaces = Space.search(params[:keyword])
+      @spaces = Space.search(params[:keyword]).where(is_active: true)
     else
-      @spaces = @q.result(distinct: true)
+      @spaces = @q.result(distinct: true).where(is_active: true)
     end
   end
 
