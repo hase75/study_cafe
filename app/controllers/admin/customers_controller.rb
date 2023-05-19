97965b0b3db6d1441c2860449cc6572b19528e3b
hase75 <122374696+hase75@users.noreply.github.com>
2023-05-19 09:40:08 +0000

    [Update]管理者の会員一覧、編集ページのレイアウト調整

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page]).per(8)
   end
 
   def show
@@ -11,17 +11,17 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     customer = Customer.find(params[:id])
     customer.update(customer_params)
     redirect_to admin_customers_path
   end
-  
+
   private
 
   def customer_params
     params.require(:customer).permit(:name, :email, :is_deleted)
   end
-  
+
 end
