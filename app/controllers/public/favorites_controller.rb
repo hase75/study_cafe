3ab6dd16fe0c22f71cc039371e188038af7535af
hase75 <122374696+hase75@users.noreply.github.com>
2023-06-28 08:56:19 +0000

    [Update]いいね機能の非同期処理実装途中

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -8,13 +8,13 @@ class Public::FavoritesController < ApplicationController
   
   def create
     @favorite = current_customer.favorites.create(space_id: params[:space_id])
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @space = Space.find(params[:space_id])
     @favorite = current_customer.favorites.find_by(space_id: @space.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 end
